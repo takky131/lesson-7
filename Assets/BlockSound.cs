c22df23a801d6d6d982ee7bd1a5f8e5e440b10eb
hiroto_takizawa <takky0131@gmail.com>
2020-08-27 21:15:13 +0900

    lesson7の課題を提出します

---
+++ Assets/BlockSound.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSound : MonoBehaviour {
+
+    public AudioClip blocksound;
+
+    public AudioSource audioSource;
+
+    // Use this for initialization
+    void Start ()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+        void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.tag == "CubeTag")
+            {
+                audioSource.PlayOneShot(blocksound);
+            }
+            else if (other.gameObject.tag == "GroundTag")
+            {
+                audioSource.PlayOneShot(blocksound);
+            }
+        }
+    
+}
